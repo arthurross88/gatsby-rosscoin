edf585170ecf0904e2c31b4ea773fa45fa1df931
Arthur Ross <arthurross@live.nl>
2021-08-05 11:02:31 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 // import Img from "gatsby-image"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
