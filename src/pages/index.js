8ab9c80311f0fe2cec0edc39493fe4d26e658913
Arthur Ross <arthurross@live.nl>
2021-08-08 15:40:04 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
     <Seo title="Home" />
     <h1>Welcome to Rosscoin.com</h1>
     <p>Here you'll find everything you need to know about Rosscoin.</p>
-    <p>Default:</p>
+    <p>Menu:</p>
   <div class="w3-bar w3-light-grey w3-border">
     <a href="#" class="w3-bar-item w3-button w3-green"><i class="fa fa-home"></i></a>
     <a href="#" class="w3-bar-item w3-button"><i class="fa fa-search"></i></a>
