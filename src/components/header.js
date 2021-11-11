a6d52fe5c2c69e7a47e4f087bd462d88ba940922
Arthur Ross <arthurross@live.nl>
2021-11-11 06:44:01 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -27,6 +27,26 @@ const Header = ({ Rosscoin }) => (
           {Rosscoin}
         </Link>
       </h1>
+      <div>
+          <nav>
+            <ul style={{ display: "flex", flex: 1 }}>
+              {menuLinks.map(link => (
+                <li
+                  key={link.name}
+                  style={{
+                    listStyleType: `none`,
+                    padding: `1rem`,
+                  }}
+                >
+                  <Link style={{ color: `white` }} to={link.link}>
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+
     </div>
   </header>
 )
