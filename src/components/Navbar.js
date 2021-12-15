916c32679cb634984222c35cf110eb6fafc43ff6
Arthur Ross <arthurross@live.nl>
2021-12-15 09:36:20 +0100

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from 'gatsby'
+import React from 'react'
+
+export default function Navbar(){
+  return(
+    <nav>
+    <h1> Web Warrior </h1>
+    <div className="links">
+    <a href=''></a>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/projects">Projects</Link>
+    </div>
+    </nav>
+    )
+}
